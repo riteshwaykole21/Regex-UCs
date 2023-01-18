199912a9aafbe0df9a0bbc8383328c73628656a2
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-18 21:42:39 +0530

    [Ritesh]add : Enter a Vaild First Name

---
+++ UserRegistrationSystem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration using Regex.\n");
+            while (true)
+            {
+                Console.WriteLine("Please select any one from below :\n" +
+                "Press 1 : To Enter your First Name.");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UserDetails.FirstName();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option selected ,Please try again !");
+                        break;
+                }
+            }
+        }
+    }
+}
